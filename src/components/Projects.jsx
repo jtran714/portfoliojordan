e18c74701d7f1764eac51176de5ed02fb921a2ff
Jordan Tran <thai.jordan@gmail.com>
2023-11-08 10:38:37 -0800

    project component in progress, doing jsx portion rn

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,77 @@
-import { useEffect } from 'react';
-import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
-import { ProjectCard } from './ProjectCard';
-import 'animate.css';
+import { useEffect } from "react";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import "animate.css";
 
 export const Projects = () => {
+  const project1 = [
+    {
+      title: "placeholder",
+      description:
+        "placeholder",
+      imgUrl: projImg2,
+    },
+    {
+      title: "laceholder",
+      description:
+        "placeholder",
+      imgUrl: projImg1,
+    },
+    {
+      title: "placeholder",
+      description:
+        "placeholder",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const project2 = [
+    {
+      title: "placeholder",
+      description:
+        "placeholder",
+      imgUrl: projImg4,
+    },
+    {
+      title: "placeholder",
+      description: "placeholder",
+      imgUrl: projImg5,
+    },
+    {
+      title: "placeholder",
+      description: "placeholder",
+      imgUrl: projImg6,
+    },
+  ];
+
+  const project3 = [
+    {
+      title: "placeholder",
+      description:
+        "placeholder",
+      imgUrl: projImg7,
+    },
+    {
+      title: "placeholders",
+      description:
+        "placeholder",
+      imgUrl: projImg8,
+    },
+    {
+      title: "placeholder",
+      description: "placeholdert",
+      imgUrl: projImg9,
+    },
+  ];
+
+  useEffect(() => {
+    const elementsToAnimate = document.querySelectorAll(".animate-on-visible");
+    elementsToAnimate.forEach((element) => {
+      element.classList.add("animate__animated", "animate__slideInLeft");
+    });
+  }, []);
+
   return (
-    <div>Projects</div>
+    
   )
-}
+};
